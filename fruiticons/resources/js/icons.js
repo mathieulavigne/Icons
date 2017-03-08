e17aaa4132d40ad8fabfd79122c3440d9ec43916
Mathieu Lavigne <mlavigne@lebleu2935.com>
2017-03-08 15:19:15 -0500

    Removed Font Awesome prefix class and maker name

--- fruiticons/resources/js/icons.js
+++ fruiticons/resources/js/icons.js
@@ -8,10 +8,10 @@
 				dropdownParent: 'body',
 			    render: {
 			        item: function(item, escape) {
-			            return '<div><i class="fa ' + escape(item.value) + '"></i>&nbsp;&nbsp;&nbsp;' + escape(item.text) + '</div>';
+			            return '<div><i class="' + escape(item.value) + '"></i>&nbsp;&nbsp;&nbsp;' + escape(item.text) + '</div>';
 			        },
 			        option: function(item, escape) {
-			            return '<div><div class="fr-option-icon"><i class="fa ' + escape(item.value) + '"></i></div><div class="fr-option-details"><div class="fr-option-name">' + escape(item.text) + '</div><div class="fr-option-maker">Font Awesome</div></div></div>';
+			            return '<div><div class="fr-option-icon"><i class="' + escape(item.value) + '"></i></div><div class="fr-option-details"><div class="fr-option-name">' + escape(item.text) + '</div><div class="fr-option-maker"></div></div></div>';
 			        }
 			    }
 			}
